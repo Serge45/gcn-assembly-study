5f7976ed9cbfdb916026a50d727bc044e639617a
Serge Lu <Serge45497@gmail.com>
2023-04-09 17:50:24 +0000

    WIP: temporarily added torch softmax for comparison

--- lesson_4/main.cpp
+++ lesson_4/main.cpp
@@ -15,28 +15,69 @@
 
 constexpr std::uint32_t NUM_WORKITEM_PER_WORKGROUP = 1024;
 
+template<typename DType, std::uint32_t Cols, std::uint32_t RowTile, std::uint32_t RowStride, std::uint32_t LdsPad>
+__device__ void globalRead(volatile DType localBuf[][Cols + LdsPad], 
+                           DType *tVal,
+                           const DType *a,
+                           std::uint32_t row,
+                           std::uint32_t col,
+                           std::uint32_t rowShift,
+                           std::uint32_t ldsPad) {
+    static_assert(Cols % RowTile == 0, "Cols % RowTile must be 0");
+
+    if constexpr (RowTile <= 2) {
+        #pragma unroll
+        for (std::uint32_t j = 0; j < RowTile; ++j) {
+            const auto procRow = row + RowStride * j;
+            const auto readOffset = blockIdx.x * blockDim.x * RowTile + procRow * Cols - rowShift * Cols + col;
+            tVal[j] = a[readOffset];
+            localBuf[procRow][col + ldsPad] = tVal[j];
+        }
+    } else if constexpr (RowTile <= 4) {
+        constexpr auto numRowComp = Cols / RowTile;
+        row = threadIdx.x / numRowComp;
+        col = (threadIdx.x % numRowComp) * RowTile;
+        const auto readOffset = blockIdx.x * blockDim.x * RowTile + row * Cols - rowShift * Cols + col;
+        float2 tVals = make_float2(a[readOffset + 0], a[readOffset + 1]);
+        tVal[0] = tVals.x;
+        tVal[1] = tVals.y;
+        //float2 *localBufPtr = (float2 *)(&localBuf[row][ldsPad]);
+        //*localBufPtr = tVals;
+        localBuf[row][col + ldsPad] = tVals.x;
+        localBuf[row][col + ldsPad + 1] = tVals.y;
+    } else {
+        constexpr auto numRowComp = Cols / RowTile;
+        row = threadIdx.x / numRowComp;
+        col = (threadIdx.x % numRowComp) * RowTile;
+        const auto readOffset = blockIdx.x * blockDim.x * RowTile + row * Cols - rowShift * Cols + col;
+        #pragma unroll
+        for (std::uint32_t j = 0; j < RowTile; ++j) {
+            tVal[j] = a[readOffset + j];
+        }
+
+        #pragma unroll
+        for (std::uint32_t j = 0; j < RowTile; ++j) {
+            localBuf[row][col + j] = tVal[j];
+        }
+    }
+}
+
 template<typename DType, std::uint32_t Cols, std::uint32_t RowTile>
 __global__ void hipGpuSoftmaxKernel(DType *m, const DType *a, std::uint32_t numRows) {
-    static_assert(Cols % 2 == 0, "Cols must be power of 2");
+    static_assert((Cols & (Cols - 1)) == 0, "Cols must be power of 2");
     constexpr uint32_t numRowsPerIter = NUM_WORKITEM_PER_WORKGROUP / Cols;
     constexpr uint32_t numRowsInBlock = RowTile * numRowsPerIter;
-    constexpr uint32_t prepad = 0;
+    constexpr uint32_t prepad = 32;
     __shared__ DType localBuf[numRowsInBlock][Cols + prepad];
     const uint32_t tId = threadIdx.x;
+    const auto lastRowIdx = numRowsInBlock * (blockIdx.x + 1);
+    const auto globalReadRowShift = (lastRowIdx <= numRows) ? 0 : (lastRowIdx - numRows);
     const auto row = tId / Cols;
     const auto col = tId & (Cols - 1);
-    const uint32_t ldsPad = 0;
+    const uint32_t ldsPad = row % prepad;
     const uint32_t paddedCol = col + ldsPad;
     DType tVal[RowTile];
-
-    #pragma unroll
-    for (std::uint32_t j = 0; j < RowTile; ++j) {
-        const auto procRow = row + numRowsPerIter * j;
-        const auto readOffset = blockIdx.x * blockDim.x * RowTile + procRow * Cols + col;
-        tVal[j] = a[readOffset];
-        localBuf[procRow][paddedCol] = tVal[j];
-    }
-
+    globalRead<DType, Cols, RowTile, numRowsPerIter, prepad>(localBuf, tVal, a, row, col, globalReadRowShift, ldsPad);
     __syncthreads();
 
     #pragma unroll
@@ -73,11 +114,14 @@ __global__ void hipGpuSoftmaxKernel(DType *m, const DType *a, std::uint32_t numR
         __syncthreads();
     }
 
+    #pragma unroll
     for (std::uint32_t j = 0; j < RowTile; ++j) {
         const auto procRow = row + j * numRowsPerIter;
-        const auto writeOffset = blockIdx.x * blockDim.x * RowTile + procRow * Cols + col;
-        const DType rowSum = localBuf[procRow][ldsPad];
-        m[writeOffset] = tVal[j] / rowSum;
+        const auto writeOffset = blockIdx.x * blockDim.x * RowTile + procRow * Cols - globalReadRowShift * Cols + col;
+        if (procRow >= globalReadRowShift) {
+            const DType rowSum = localBuf[procRow][ldsPad];
+            m[writeOffset] = tVal[j] / rowSum;
+        }
     }
 }
 
@@ -128,15 +172,23 @@ int main(int argc, char **argv) {
     err = hipEventCreate(&end);
 
     err = hipEventRecord(beg);
-    std::size_t numRuns = 100;
+
+    constexpr std::size_t numWarmups = 50;
+    constexpr std::size_t rowTile = 1;
+
+    for (std::size_t i = 0; i < numWarmups; ++i) {
+        hipGpuSoftmax<float, n, rowTile>(hipResult, gpuMem, m);
+    }
+
+    const std::size_t numRuns = 300;
     for (std::size_t i = 0; i < numRuns; ++i) {
-        hipGpuSoftmax<float, n, 2>(hipResult, gpuMem, m);
+        hipGpuSoftmax<float, n, rowTile>(hipResult, gpuMem, m);
     }
     err = hipDeviceSynchronize();
     err = hipEventRecord(end);
     float hipDur{};
     err = hipEventElapsedTime(&hipDur, beg, end);
-    std::cout << "HIP Softmax func: " << std::to_string(hipDur / numRuns) << " ms ~= " << 2 * numElements * sizeof(float) * 1e3 / std::pow(1024.f, 3) / hipDur << " GB/s\n";
+    std::cout << "HIP Softmax func: " << std::to_string(hipDur / numRuns) << " ms ~= " << 2 * numRuns * numElements * sizeof(float) * 1e3 / std::pow(1024.f, 3) / hipDur << " GB/s\n";
 
     auto cpuBeg = std::chrono::steady_clock::now();
     std::vector<float> cpuOut(numElements);
@@ -151,7 +203,12 @@ int main(int argc, char **argv) {
 
     for (size_t i = 0; i < numElements; ++i) {
         if (!almostEqual(hipResult[i], cpuOut[i])) {
-            //std::cout << "Check HIP vs CPU failed at: " << i << ", " << std::to_string(hipReturnedResult[i]) << " : " << std::to_string(cpuOut[i]) << '\n';
+            // std::cout << "Check HIP vs CPU failed at: " << i
+            //     << ", " << std::to_string(hipReturnedResult[i])
+            //     << " : " << std::to_string(cpuOut[i])
+            //     << ", diff: "
+            //     << std::to_string(std::abs(hipReturnedResult[i] - cpuOut[i]))
+            //     << '\n';
             ++numMismatched;
         }
     }
---
+++ lesson_4/torch_softmax.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+assert (torch.has_cuda and torch.cuda.is_available()) is True
+
+d = torch.device('cuda:0' if torch.has_cuda else 'cpu')
+assert torch.cuda.current_device() == 0
+m, n = 1335, 16
+
+s = nn.Softmax(dim=-1)
+i = torch.randn(m, n, dtype=torch.float, device=d, requires_grad=False)
+o = torch.zeros(m, n, dtype=torch.float, device=d, requires_grad=False)
+
+for _ in range(100):
+    o = s(i)
+
+print(o)
